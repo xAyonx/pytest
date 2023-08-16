d350ee2d491bb6a36d13e668ac60159d7d9785eb
Ayon <115979462+xAyonx@users.noreply.github.com>
2023-08-16 15:49:30 +0200

    Update start_run.py

--- start_run.py
+++ start_run.py
@@ -1,5 +1,5 @@
 import unittest
-from start_run import add
+from start import add
 class TestAdd(unittest.TestCase):
     def test_add_function(self):
         self.assertEqual(add(1, 2), 2)
@@ -7,8 +7,7 @@ class TestAdd(unittest.TestCase):
     def test_add_function_with_floats(self):
         self.assertAlmostEqual(add(2.1, 5.2), 10.92)
 
-    def test_add_function_with_floats_wrong(self):
-        self.assertEqual(add(2.1, 5.2), 10.92)
+    
 
 
 if __name__ == "__main__":
