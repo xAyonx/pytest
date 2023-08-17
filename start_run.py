3bd63d1d904f39d98db7dfa5d19edd2eba671b5b
Ayon <115979462+xAyonx@users.noreply.github.com>
2023-08-17 14:12:48 +0200

    update2

--- start_run.py
+++ start_run.py
@@ -1,11 +1,9 @@
 import unittest
 from start import add
+
 class TestAdd(unittest.TestCase):
     def test_add_function(self):
-        self.assertEqual(add(1, 2), 2)
-        self.assertEqual(add(4, 7), 28)
-    def test_add_function_with_floats(self):
-        self.assertAlmostEqual(add(2.1, 5.2), 10.92)
+        self.assertEqual(add(5, 5), 25)
 
     
 
